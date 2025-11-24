c6c944c1c5330d072fff471e3511d3e65f66154a
Basti <ernst.sebastian1991@mail.com>
2025-11-25 00:07:01 +0100

    feat(utils): Add function to check if an ability is in range

--- utils/combat.lua
+++ utils/combat.lua
@@ -3,3 +3,18 @@ function combat_StartAutoAttack()
         AttackTarget()
     end
 end
+
+function combat_isTextureNameInRange(textureName)
+    for i = 1, 172 do
+        local isSameTexture = strfind(GetActionTexture(i), textureName)
+        if isSameTexture then
+            if IsActionInRange(i) == 1 then
+                return true
+            else
+                return false
+            end
+        end
+    end
+
+    return false
+end
